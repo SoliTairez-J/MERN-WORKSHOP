55855ce3f856374148b2d095a042aef8435a96f6
Warawit Siriyong <warawit.j@gmail.com>
2021-11-13 00:06:50 +0700

    finish commit

---
+++ client/src/AdminRoute.js
@@ -0,0 +1,16 @@
+import { Redirect, Route } from "react-router-dom";
+import { getUser } from "./services/authorize";
+
+const AdminRoute=({component:Component,...rest})=>(
+    <Route
+        {...rest}
+        render={props=>
+            getUser() ? (<Component {...props}/>) : 
+            (<Redirect 
+                to={{pathname:"/login",state:{from:props.location}}}
+            />)
+        }
+    />
+)
+
+export default AdminRoute
